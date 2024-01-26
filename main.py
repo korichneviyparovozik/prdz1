c33f1b5b1fa13030d8cf4cef6e0de98ed258b247
Миша <vorono594@gmail.com>
2024-01-26 22:15:41 +0100

    com

---
+++ main.py
@@ -0,0 +1,19 @@
+class Films:
+    Duration = "long"
+    Episodes = "less than 3"
+    Plot = "preconceived"
+
+    def __init__(self):
+        self.name = "fight club"
+        self.year = "1999"
+        self.main_character = "Narrator"
+
+class Series:
+    duration = "short"
+    episodes = "more than 3"
+    plot = "made up on the fly"
+
+    def __init__(self):
+        self.name = "Simpsons"
+        self.year = "1989"
+        self.main_character = "Simpson Family"
